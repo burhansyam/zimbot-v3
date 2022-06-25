92693c70ec1a54e5edc7828752a4d111efbd535d
Burhan Syam <burhansyam85@gmail.com>
2022-06-25 15:53:47 +0700

    Update zimbot.js
    Signed-off-by: Burhan Syam <burhansyam85@gmail.com>

--- zimbot.js
+++ zimbot.js
@@ -1,35 +1,3 @@
-
-// ⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈
-//▮ZIM BOT INC 2022 ®️ALL RIGHTS RESERVED
-//▮
-//▮FORK AND DON'T FORGET TO GIVE A STAR
-//▮
-//▮THIS SOFTWARE IS UNDER UZ COPYRIGHT
-//▮
-//▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
-//▮reinhardtuna@mail.uk
-//▮WHATSAPP US : +44 7441 437150
-//▮YOUTUBE CHANNELL: https://youtube.com/c/DRIPSOFC
-//▮
-//╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-//
-//┏━━━━━━━━━━━━━━━━━━━━━━━━━
-//┃THIS SOFTWARE INCLUDES 
-//┃SOME ENCRYPTED FILES
-//┃
-//┃THANKS FOR CHOOSING ZIMBOT
-//┃THANKS TO DikaArdnt
-//┃BEHOLD THE NEW
-//┃
-//┃LET LOVE LEAD
-//┃
-//┃YOU WANNA RE UPLOAD GIVE A PROPER CREDIT
-//┗━━━━━━━━━━━━━━━━━━━━━━━━━
-//
-
-
-
-
 process.on('uncaughtException', console.error) //Safe Log Error
 
 require('./config')
